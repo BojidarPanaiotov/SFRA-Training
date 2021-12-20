81e79d3ec90f57541a99195d127702813e4cdef0
Bozhidar Panayotov <cccpisthebest@abv.bg>
2021-12-20 09:53:38 +0200

    Fixed content slot which list categories
    Removed the logic for registered/unregistered customers, from the controller and now it can be changed easily in the content slot

--- storefront-reference-architecture/cartridges/sfra_training/cartridge/controllers/NewHome.js
+++ storefront-reference-architecture/cartridges/sfra_training/cartridge/controllers/NewHome.js
@@ -10,38 +10,9 @@ server.get('Show', function (req, res, next) {
     next();
 });
 
-
 server.get('ListCategories', function (req, res, next) {
-    var womensCategory = CatalogMgr.getCategory('womens').subCategories;
-    var mensCategory = CatalogMgr.getCategory('mens-clothing').subCategories;
-
-    var womensResult = [];
-    var mensResult = [];
-
-    for (let index = 0; index < womensCategory.length; index++) {
-        var data = {
-            name: womensCategory[index].displayName,
-            description: womensCategory[index].pageDescription,
-            image: womensCategory[index].custom.slotBannerImage.httpsURL
-        };
-
-        womensResult.push(data);
-    }
-
-    for (let index = 0; index < mensCategory.length; index++) {
-        var data = {
-            name: mensCategory[index].displayName,
-            description: mensCategory[index].pageDescription,
-            image: mensCategory[index].custom.slotBannerImage.httpsURL
-        };
-
-        mensResult.push(data);
-    }
     
-    res.render('listCategories', {
-        womensResult: womensResult,
-        mensResult: mensResult,
-    });
+    res.render('listCategories');
 
     next();
 });
